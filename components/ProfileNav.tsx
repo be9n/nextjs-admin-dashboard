7e9dc6ccd818fc8d2d7cf7734c868675d7165cd0
Abdulrahmannas <a.nabbout@fromscratch-solutions.com>
2025-05-06 18:26:34 +0300

    Updates

--- components/ProfileNav.tsx
+++ components/ProfileNav.tsx
@@ -22,30 +22,28 @@ export default function ProfileNav() {
   const { user, logout } = useAuth();
 
   return (
-    <div className="h-8 w-8 rounded-full bg-indigo-100 flex items-center justify-center cursor-pointer">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <span className="text-indigo-800 font-medium">
-            {user?.name.charAt(0).toString().toUpperCase() || "U"}
-          </span>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="mt-2 me-2">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          {accountLinks.map((link) => (
-            <DropdownMenuItem asChild key={link.to}>
-              <Link href={"/dashboard/profile"} className="cursor-pointer">
-                Profile
-              </Link>
-            </DropdownMenuItem>
-          ))}
-          <DropdownMenuSeparator />
-          <DropdownMenuItem asChild>
-            <button className="w-full cursor-pointer" onClick={logout}>
-              Logout
-            </button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <span className="text-indigo-800 font-medium h-9 w-9 rounded-full bg-indigo-100 flex items-center justify-center cursor-pointer">
+          {user?.name.charAt(0).toString().toUpperCase() || "U"}
+        </span>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="mt-2 me-2">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        {accountLinks.map((link) => (
+          <DropdownMenuItem asChild key={link.to}>
+            <Link href={"/dashboard/profile"} className="cursor-pointer">
+              Profile
+            </Link>
           </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+        ))}
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <button className="w-full cursor-pointer" onClick={logout}>
+            Logout
+          </button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
